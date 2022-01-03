f558885794c13be3f653209e627d97bdbd14e7d9
Jesper Stemann Andersen <jsa@ihp.dk>
2022-01-03 20:48:28 +0100

    Added method for getting Argus version as string

--- src/ArgusWrapper.jl
+++ src/ArgusWrapper.jl
@@ -1,3 +1,10 @@
 module ArgusWrapper
 
+using CxxWrap
+@wrapmodule(joinpath(@__DIR__, "..", "build", "lib", "libargus_julia_wrapper"), :define_argus_wrapper)
+
+function __init__()
+    @initcxx
+end
+
 end # module
--- src/argus_wrapper.cpp
+++ src/argus_wrapper.cpp
@@ -3,3 +3,15 @@
 #include "jlcxx/jlcxx.hpp"
 #include "jlcxx/functions.hpp"
 #include "uv.h"
+
+JLCXX_MODULE define_argus_wrapper(jlcxx::Module& module)
+{
+  using namespace Argus;
+
+  module.method("get_argus_version_string", []()
+  {
+    UniqueObj<CameraProvider> cameraProvider(CameraProvider::create());
+    ICameraProvider *iCameraProvider = interface_cast<ICameraProvider>(cameraProvider);
+    return iCameraProvider->getVersion();
+  });
+}
